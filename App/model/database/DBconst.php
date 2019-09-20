65fd0958466deb9eb2929012122e0e43c3a79846
Dike Pascal Ozioma <dikep15@gmail.com>
2019-09-21 00:24:51 +0100

    Update DBconst.php

--- App/model/database/DBconst.php
+++ App/model/database/DBconst.php
@@ -5,7 +5,7 @@
     class DBconst {
         protected const DB_SERVER = "db4free.net";
         protected const DB_USER = "ozioma";
-        protected const DB_PASSWORD = "ozioma19";
+        protected const DB_PASSWORD = "pascal19";
         protected const DB_NAME = "user_reg";
     }
 ?>
