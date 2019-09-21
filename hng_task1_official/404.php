69f69efc820a47108ac47946abca0a0380ba3c65
jeawhanlee <38788055+jeawhanlee@users.noreply.github.com>
2019-09-21 03:59:04 +0100

    Latest Backend update with the latest UI
    HNG Task 1 - Simple Sign in. The folder 'hng_task1_official' has the latest update.

---
+++ hng_task1_official/404.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Les Incroyables - Landing Page</title>
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--=====================CSS=================================-->
+ <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet"> 
+    
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/main.css">
+	<style type="text/css">
+	</style>
+</head>
+<body id="landing">
+		<div class="mycontent">
+		<div class="overlay">
+			<div class="container-fluid on-web fullvw">
+					<div class="col-md-12 body-nofooter fullvw header">
+						<nav class=" fullvw-1 navbar up navbar-expand-lg">
+			 				<div class="col-md-5 col-sm-5 site-title1">
+							</div>
+							<div class="col-md-2 logo1 col-sm-2 float-left weblogo">
+								<img src="assets/logo-0.png" height="50">
+							</div>
+							
+							<div class="col-md-5 col-sm-5 float-right webmenu">
+						</nav>
+
+
+						<div class="col-md-12">
+							<div class="col-md-8 content-side" style="z-index: 1">
+								<div class="made-texts">
+									<div class="col-12"><h1>404 ERROR</h1>
+									<h4>Page Not Found</h4>
+									<a href="#" class="btn forgot" ><b style="color:#F0D90C"><< Back Home</b></a>
+									</div>	
+								</div>
+							</div>
+						</div>
+</div>
+
+				 <footer class=" fullvw-1 navbar down navbar-expand-lg">
+				 	<a class="made" href="">© 2019 Les Incroyables. All Rights Reserved.</a>
+				 </footer>
+		</div>
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script type="text/javascript" src="Resources/js/made.js"></script>
+</body>
+</html>
